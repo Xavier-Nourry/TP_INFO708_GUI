5c9366965aacfcc7b69ae46ec59ef888793ab476
Xavier-Nourry <xavier.nourry@etu.univ-smb.fr>
2021-01-14 15:10:22 +0100

    view : set the parent class of main frame as JFrame instead of Frame to set the default close operation (by clicking on window's close arrow)

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -2,7 +2,9 @@ package TP_INFO708_GUI.src.view;
 
 import java.awt.*;
 
-public class MainFrame extends Frame{
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame{
     public MainFrame(){
         setSize(800, 600);
         setTitle("Système de drainage dans une mine");
@@ -22,6 +24,7 @@ public class MainFrame extends Frame{
         add(CH4_Label);
         add(CH4_LevelLabel);
         
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
 }
