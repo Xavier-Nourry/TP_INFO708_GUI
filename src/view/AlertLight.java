f36a39d5210413a552ed1956f5cedb57ee6805f6
Xavier-Nourry <xavier.nourry@etu.univ-smb.fr>
2021-01-14 16:32:36 +0100

    view : add of containers for UI elements & add of lights with colors

---
+++ src/view/AlertLight.java
@@ -0,0 +1,28 @@
+package TP_INFO708_GUI.src.view;
+
+import java.awt.*;
+
+public class AlertLight extends Canvas{
+    public Color colorOn;
+    public Color colorOff;
+    private Color currentColor;
+
+    public AlertLight(Color color){ 
+        this.colorOn = color; 
+        this.colorOff = colorOn.darker();
+        this.currentColor = this.colorOff;
+        setBackground(Color.WHITE);
+        setSize(32, 32);
+    }
+
+    public void paint(Graphics g){
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillOval(0, 0, 32, 32);
+        g.setColor(this.currentColor);
+        g.fillOval(5, 5, 22, 22);
+    }
+
+    public void setCurrentColorToOn() { this.currentColor = this.colorOn; }
+
+    public void setCurrentColorToOff() { this.currentColor = this.colorOff; }
+}
--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -1,29 +1,60 @@
 package TP_INFO708_GUI.src.view;
 
 import java.awt.*;
-
 import javax.swing.JFrame;
+import TP_INFO708_GUI.src.view.AlertLight;
 
 public class MainFrame extends JFrame{
     public MainFrame(){
-        setSize(800, 600);
+        /*################# Configuration de la fenêtre #################*/
+        setSize(300, 300);
         setTitle("Système de drainage dans une mine");
-        setLayout(new GridLayout(5,4));
+        setLayout(new GridLayout(5,1));
 
+        /*################# EAU #################*/
         Label waterLabel = new Label("Niveau d'eau :");
         Label waterLevelLabel = new Label("0.0f");
+        AlertLight waterDecreasingLevelAlert = new AlertLight(Color.CYAN);
+        AlertLight waterIncreasingLevelAlert = new AlertLight(Color.CYAN);
+        FlowLayout waterElementsLayout = new FlowLayout();
+        Container waterElementsContainer = new Container();
+        waterElementsContainer.setLayout(waterElementsLayout);
+        waterElementsContainer.add(waterLabel, null);
+        waterElementsContainer.add(waterLevelLabel, null);
+        waterElementsContainer.add(waterDecreasingLevelAlert, null);
+        waterElementsContainer.add(waterIncreasingLevelAlert, null);
+        
+        /*################# CO #################*/
         Label CO_Label = new Label("Niveau de CO :");
         Label CO_LevelLabel = new Label("0.0f");
+        AlertLight CO_DecreasingLevelAlert = new AlertLight(Color.GREEN);
+        AlertLight CO_IncreasingLevelAlert = new AlertLight(Color.GREEN);
+        FlowLayout CO_ElementsLayout = new FlowLayout();
+        Container CO_ElementsContainer = new Container();
+        CO_ElementsContainer.setLayout(CO_ElementsLayout);
+        CO_ElementsContainer.add(CO_Label, null);
+        CO_ElementsContainer.add(CO_LevelLabel, null);
+        CO_ElementsContainer.add(CO_DecreasingLevelAlert, null);
+        CO_ElementsContainer.add(CO_IncreasingLevelAlert, null);
+
+        /*################# CH4 #################*/
         Label CH4_Label = new Label("Niveau de CH4 :");
         Label CH4_LevelLabel = new Label("0.0f");
+        AlertLight CH4_DecreasingLevelAlert = new AlertLight(Color.YELLOW);
+        AlertLight CH4_IncreasingLevelAlert = new AlertLight(Color.YELLOW);
+        FlowLayout CH4_ElementsLayout = new FlowLayout();
+        Container CH4_ElementsContainer = new Container();
+        CH4_ElementsContainer.setLayout(CH4_ElementsLayout);
+        CH4_ElementsContainer.add(CH4_Label, null);
+        CH4_ElementsContainer.add(CH4_LevelLabel, null);
+        CH4_ElementsContainer.add(CH4_DecreasingLevelAlert, null);
+        CH4_ElementsContainer.add(CH4_IncreasingLevelAlert, null);
         
-        add(waterLabel);
-        add(waterLevelLabel);
-        add(CO_Label);
-        add(CO_LevelLabel);
-        add(CH4_Label);
-        add(CH4_LevelLabel);
-        
+        /*################# Ajout des éléments à la fenêtre #################*/
+        add(waterElementsContainer);
+        add(CH4_ElementsContainer);
+        add(CO_ElementsContainer);
+    
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
